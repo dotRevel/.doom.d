e9fe40b2f20c77714b9da4abe01f1d653dc321bb
dotRevel <126297648+dotRevel@users.noreply.github.com>
2024-01-05 13:07:36 -0400

    Add files via upload

---
+++ config.el
@@ -0,0 +1,147 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+ (set-frame-parameter (selected-frame)'alpha '(92 . 92))
+ (add-to-list 'default-frame-alist'(alpha . (92 . 92)))
+(setq jit-lock-stealth-time 1.25)
+(setq jit-lock-stealth-nice 0.5) ;; Seconds between font locking.
+(setq jit-lock-chunk-size 4096)
+(setq jit-lock-defer-time 0)
+(with-eval-after-load 'evil
+  (add-hook 'evil-insert-state-entry-hook
+    (lambda ()
+      (setq jit-lock-defer-time 0.25)) nil t)
+  (add-hook 'evil-insert-state-exit-hook
+    (lambda ()
+      (setq jit-lock-defer-time 0)) nil t))
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+;;
+;;;; lsp
+(after! lsp-mode
+  (setq lsp-inlay-hint-enable t)
+  (setq  lsp-auto-guess-root nil)
+  )
+;; Languages - C/C++
+
+(setq c-default-style "k&r"
+      c-basic-offset 4
+      indent-tabs-mode nil)
+
+
+;;; native compilation
+(setq native-comp-async-jobs-number 5) ; use 5 cores when native compiling
+
+;;; compilation - hide compilation if successful
+
+ (add-hook 'compilation-start-hook 'compilation-started)
+  (add-hook 'compilation-finish-functions 'hide-compile-buffer-if-successful)
+
+  (defcustom auto-hide-compile-buffer-delay 1
+    "Time in seconds before auto hiding compile buffer."
+    :group 'compilation
+    :type 'number
+  )
+
+  (defun hide-compile-buffer-if-successful (buffer string)
+    (setq compilation-total-time (time-subtract nil compilation-start-time))
+    (setq time-str (concat " (Time: " (format-time-string "%s.%3N" compilation-total-time) "s)"))
+
+    (if
+      (or
+        (string-match ".*exited abnormally.*" string)
+
+        ;; Delete it, if Hide Warnings too.
+        (with-current-buffer buffer
+          (goto-char (point-min))
+          (search-forward "warning" nil t)
+        )
+      )
+
+      ;;If Warnings/Errors then
+      (message (concat "Compiled with Warnings/Errors" time-str))
+
+      ;;If Compiled Successfully then
+      (progn
+        (bury-buffer buffer)
+        (run-with-timer auto-hide-compile-buffer-delay nil 'delete-window (get-buffer-window buffer 'visible))
+        (message (concat "Compiled Successfully" time-str))
+        (shell-command "notify-send emacs-compilation 'Compilation completed'")
+      )
+    )
+  )
+
+  (make-variable-buffer-local 'compilation-start-time)
+
+  (defun compilation-started (proc)
+    (setq compilation-start-time (current-time))
+  )
+
+
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+(setq doom-font (font-spec :family "JetBrains Mono Nerd Font" :size 20 :weight 'Regular))
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-solarized-dark-high-contrast)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Sync/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("ffafb0e9f63935183713b204c11d22225008559fa62133a69848835f4f4a758c" "9013233028d9798f901e5e8efb31841c24c12444d3b6e92580080505d56fd392" "10e5d4cc0f67ed5cafac0f4252093d2119ee8b8cb449e7053273453c1a1eb7cc" "37b6695bae243145fa2dfb41440c204cd22833c25cd1993b0f258905b9e65577" "13096a9a6e75c7330c1bc500f30a8f4407bd618431c94aeab55c9855731a95e1" "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "3cdd0a96236a9db4e903c01cb45c0c111eb1492313a65790adb894f9f1a33b2d" default))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(package-selected-packages '(arduino-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ts-fold-replacement-face ((t (:foreground unspecified :box nil :inherit font-lock-comment-face :weight light)))))
